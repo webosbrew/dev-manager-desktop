b9679f4b14751b421b73c2f34b7f306b526ec325
Mariotaku <mariotaku.lee@gmail.com>
2021-07-30 12:21:19 +0900

    Updated dev mode hint

--- src/app/shared/util/cors-skip.ts
+++ src/app/shared/util/cors-skip.ts
@@ -2,7 +2,6 @@ import { HeadersReceivedResponse, OnHeadersReceivedListenerDetails } from "elect
 
 export function AllowCORSHandler(details: OnHeadersReceivedListenerDetails, callback: (resp: HeadersReceivedResponse) => void): void {
   details.responseHeaders['access-control-allow-origin'] = ['*'];
-  console.log({ ...details.responseHeaders });
   callback({
     cancel: false,
     responseHeaders: { ...details.responseHeaders }
