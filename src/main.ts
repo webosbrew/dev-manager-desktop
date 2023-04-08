15a07486115129c1d275c0b91e3e2a00d67f82c8
Mariotaku <mariotaku.lee@gmail.com>
2023-04-08 23:46:40 +0900

    updated build workflow

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ Sentry.init({
     })
   ],
   environment: AppConfig.environment,
-  release: `ui@${packageInfo.version}`,
+  release: packageInfo.version,
   beforeBreadcrumb: (breadcrumb, hint) => breadcrumb.level !== 'debug' ? breadcrumb : null,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
