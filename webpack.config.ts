f49e17516e42cbea014a15565cea8c056bfdf568
Mariotaku <mariotaku.lee@gmail.com>
2023-06-20 14:02:27 +0900

    removed unused logging

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,9 @@
 //Polyfill Node.js core modules in Webpack. This module is only needed for webpack 5+.
 import {BuildOptions} from "@angular-devkit/build-angular/src/utils/build-options";
 import type {Configuration} from 'webpack';
-import * as util from "util";
 
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const {IgnorePlugin} = require("webpack");
-const path = require('path');
 
 /**
  * Custom angular webpack configuration
@@ -46,7 +44,6 @@ module.exports = (config: Configuration, options: BuildOptions) => {
   }
 
   config.module.rules.push({test: /\.sh/, type: 'asset/source'});
-  console.log(util.inspect(config));
 
   return config;
 };
