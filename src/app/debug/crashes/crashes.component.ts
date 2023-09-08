dc90a4d1b1b8be89059dc42e0f41defecc3ebada
Mariotaku <mariotaku.lee@gmail.com>
2023-09-08 21:51:40 +0900

    supports delete crash report

--- src/app/debug/crashes/crashes.component.ts
+++ src/app/debug/crashes/crashes.component.ts
@@ -3,53 +3,72 @@ import {CrashReport, DeviceManagerService} from '../../core/services';
 import {Device} from "../../types";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {DetailsComponent} from "./details/details.component";
+import {MessageDialogComponent} from "../../shared/components/message-dialog/message-dialog.component";
+import {RemoteFileService} from "../../core/services/remote-file.service";
+import {remove} from "lodash";
 
 @Component({
-  selector: 'app-crashes',
-  templateUrl: './crashes.component.html',
-  styleUrls: ['./crashes.component.scss']
+    selector: 'app-crashes',
+    templateUrl: './crashes.component.html',
+    styleUrls: ['./crashes.component.scss']
 })
 export class CrashesComponent {
 
-  private deviceField: Device | null = null;
+    private deviceField: Device | null = null;
 
-  reports?: CrashReport[];
-  reportsError?: Error;
+    reports?: CrashReport[];
+    reportsError?: Error;
 
-  constructor(
-    public deviceManager: DeviceManagerService,
-    private modals: NgbModal
-  ) {
-  }
-
-  get device(): Device | null {
-    return this.deviceField;
-  }
-
-  @Input()
-  set device(device: Device | null) {
-    this.deviceField = device;
-    this.reload();
-  }
-
-  public reload() {
-    if (this.deviceField) {
-      this.deviceManager.listCrashReports(this.deviceField).then(reports => this.reports = reports)
-        .catch(e => this.reportsError = e);
-    } else {
-      this.reports = undefined;
+    constructor(
+        public deviceManager: DeviceManagerService,
+        public files: RemoteFileService,
+        private modals: NgbModal
+    ) {
     }
-  }
 
-  openDetails(report: CrashReport) {
-    this.modals.open(DetailsComponent, {
-      size: 'lg',
-      scrollable: true,
-      injector: Injector.create({
-        providers: [{
-          provide: CrashReport, useValue: report
-        }]
-      })
-    });
-  }
+    get device(): Device | null {
+        return this.deviceField;
+    }
+
+    @Input()
+    set device(device: Device | null) {
+        this.deviceField = device;
+        this.reload();
+    }
+
+    public reload() {
+        if (this.deviceField) {
+            this.deviceManager.listCrashReports(this.deviceField).then(reports => this.reports = reports)
+                .catch(e => this.reportsError = e);
+        } else {
+            this.reports = undefined;
+        }
+    }
+
+    openDetails(report: CrashReport) {
+        this.modals.open(DetailsComponent, {
+            size: 'lg',
+            scrollable: true,
+            injector: Injector.create({
+                providers: [{
+                    provide: CrashReport, useValue: report
+                }]
+            })
+        });
+    }
+
+    async deleteReport(report: CrashReport) {
+        const positive = await MessageDialogComponent.open(this.modals, {
+            title: 'Delete Crash Report',
+            message: `Are you sure you want to delete this crash report?`,
+            positive: 'Delete',
+            positiveStyle: 'danger',
+            negative: 'Cancel',
+        }).result.catch(() => false);
+        if (!positive) {
+            return;
+        }
+        await this.files.rm(report.device, report.path, false)
+            .then(() => this.reports && remove(this.reports, r => r.path === report.path))
+    }
 }
--- src/app/types/file-session.ts
+++ src/app/types/file-session.ts
@@ -1,34 +1,41 @@
 export type FileType = '-' | 'd' | 'c' | 'b' | 's' | 'p' | 'l' | '';
 
 export declare interface FileItem {
-  filename: string;
-  type: FileType;
-  mode: string;
-  user?: string;
-  group?: string;
-  size: number,
-  mtime: number,
-  link?: LinkInfo;
+    filename: string;
+    type: FileType;
+    mode: string;
+    user?: string;
+    group?: string;
+    size: number,
+    mtime: number,
+    link?: LinkInfo;
+    access?: PermInfo;
 }
 
 export declare interface LinkInfo {
-  target?: string;
-  broken?: boolean;
+    target?: string;
+    broken?: boolean;
+}
+
+export declare interface PermInfo {
+    read: boolean;
+    write: boolean;
+    execute: boolean;
 }
 
 export declare interface FileSession {
 
-  ls(path: string): Promise<FileItem[]>;
+    ls(path: string): Promise<FileItem[]>;
 
-  rm(path: string, recursive: boolean): Promise<void>;
+    rm(path: string, recursive: boolean): Promise<void>;
 
-  get(remotePath: string, localPath: string): Promise<void>;
+    get(remotePath: string, localPath: string): Promise<void>;
 
-  put(localPath: string, remotePath: string): Promise<void>;
+    put(localPath: string, remotePath: string): Promise<void>;
 
-  mkdir(path: string): Promise<void>;
+    mkdir(path: string): Promise<void>;
 
-  getTemp(remotePath: string): Promise<string>;
+    getTemp(remotePath: string): Promise<string>;
 
-  uploadBatch(strings: string[], pwd: string, failCb: (name: string, e: Error) => Promise<boolean>): Promise<void>;
+    uploadBatch(strings: string[], pwd: string, failCb: (name: string, e: Error) => Promise<boolean>): Promise<void>;
 }
