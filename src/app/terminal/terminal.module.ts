a922b85a5c05be1723d0ba38cc334900b0a5e381
Mariotaku <mariotaku.lee@gmail.com>
2023-10-05 15:23:44 +0900

    updated deps

--- src/app/terminal/terminal.module.ts
+++ src/app/terminal/terminal.module.ts
@@ -6,26 +6,26 @@ import {NgbDropdownModule, NgbNavModule} from "@ng-bootstrap/ng-bootstrap";
 import {TerminalComponent} from "./terminal.component";
 import {PtyComponent} from "./pty/pty.component";
 import {DumbComponent} from './dumb/dumb.component';
-import {TextareaAutosizeModule} from "ngx-textarea-autosize";
 import {FormsModule} from "@angular/forms";
 import {SharedModule} from "../shared/shared.module";
+import {AutosizeModule} from "ngx-autosize";
 
 
 @NgModule({
-  declarations: [
-    TerminalComponent,
-    PtyComponent,
-    DumbComponent,
-  ],
-  imports: [
-    CommonModule,
-    NgbNavModule,
-    TerminalRoutingModule,
-    TextareaAutosizeModule,
-    NgbDropdownModule,
-    FormsModule,
-    SharedModule,
-  ]
+    declarations: [
+        TerminalComponent,
+        PtyComponent,
+        DumbComponent,
+    ],
+    imports: [
+        CommonModule,
+        NgbNavModule,
+        TerminalRoutingModule,
+        AutosizeModule,
+        NgbDropdownModule,
+        FormsModule,
+        SharedModule,
+    ]
 })
 export class TerminalModule {
 }
