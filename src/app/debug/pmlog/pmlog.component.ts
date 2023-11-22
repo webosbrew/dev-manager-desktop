fa84525e5b8dcad8496ad0a49c9e0d646d635c95
Mariotaku <mariotaku.lee@gmail.com>
2023-11-22 12:36:15 +0900

    properly displays webOS release version

--- src/app/debug/pmlog/pmlog.component.ts
+++ src/app/debug/pmlog/pmlog.component.ts
@@ -11,91 +11,91 @@ import {RemoteLunaService} from "../../core/services/remote-luna.service";
 import semver from "semver/preload";
 
 @Component({
-  selector: 'app-pmlog',
-  templateUrl: './pmlog.component.html',
-  styleUrls: ['./pmlog.component.scss']
+    selector: 'app-pmlog',
+    templateUrl: './pmlog.component.html',
+    styleUrls: ['./pmlog.component.scss']
 })
 export class PmLogComponent {
 
-  logs?: Observable<LogMessage>;
-  logError?: Error;
-  hasData?: boolean;
+    logs?: Observable<LogMessage>;
+    logError?: Error;
+    hasData?: boolean;
 
-  private deviceField: Device | null = null;
+    private deviceField: Device | null = null;
 
-  constructor(private cmd: RemoteCommandService, private log: RemoteLogService,
-              private luna: RemoteLunaService, private deviceManager: DeviceManagerService,
-              private modals: NgbModal) {
-  }
-
-
-  get device(): Device | null {
-    return this.deviceField;
-  }
-
-  @Input()
-  set device(device: Device | null) {
-    this.deviceField = device;
-    this.logs = undefined;
-    if (device) {
-      this.reload(device);
+    constructor(private cmd: RemoteCommandService, private log: RemoteLogService,
+                private luna: RemoteLunaService, private deviceManager: DeviceManagerService,
+                private modals: NgbModal) {
     }
-  }
 
-  private reload(device: Device) {
-    this.logError = undefined;
-    this.hasData = undefined;
-    this.logs = from(this.logRead(device)).pipe(mergeMap(identity), tap(message => {
-      if (this.hasData === undefined) {
-        this.hasData = true;
-      }
-    }), catchError(err => {
-      this.logError = err;
-      if (this.hasData === undefined) {
-        this.hasData = false;
-      }
-      throw err;
-    }));
-  }
 
-  private async logRead(device: Device): Promise<Observable<LogMessage>> {
-    const info = await this.deviceManager.getSystemInfo(device);
-    if (semver.satisfies(info.sdkVersion || '', '>=4.0')) {
-      await this.luna.call(device, 'luna://com.webos.service.config/setConfigs', {configs: {"system.collectDevLogs": true}}, false);
-    } else if (semver.satisfies(info.sdkVersion || '', '>=2.0')) {
-      await this.luna.call(device, 'luna://com.webos.pmlogd/setdevlogstatus', {recordDevLogs: true}, false);
+    get device(): Device | null {
+        return this.deviceField;
     }
-    return this.log.logread(device, 100);
-  }
 
-  async openCtrl(): Promise<void> {
-    const device = this.device;
-    if (!device) {
-      return;
+    @Input()
+    set device(device: Device | null) {
+        this.deviceField = device;
+        this.logs = undefined;
+        if (device) {
+            this.reload(device);
+        }
     }
-    const progress = ProgressDialogComponent.open(this.modals);
-    try {
-      const contexts = await this.log.pmLogShow(device);
-      this.modals.open(PmLogControlComponent, {
-        injector: Injector.create({
-          providers: [
-            {provide: 'device', useValue: this.device},
-            {provide: 'contexts', useValue: contexts}
-          ]
-        }),
-        scrollable: true,
-      });
-    } finally {
-      progress.close();
-    }
-  }
 
-  async clearBuffer(): Promise<void> {
-    const device = this.device;
-    if (!device) {
-      return;
+    private reload(device: Device) {
+        this.logError = undefined;
+        this.hasData = undefined;
+        this.logs = from(this.logRead(device)).pipe(mergeMap(identity), tap(() => {
+            if (this.hasData === undefined) {
+                this.hasData = true;
+            }
+        }), catchError(err => {
+            this.logError = err;
+            if (this.hasData === undefined) {
+                this.hasData = false;
+            }
+            throw err;
+        }));
     }
-    await this.log.logClear(device);
-    this.reload(device);
-  }
+
+    private async logRead(device: Device): Promise<Observable<LogMessage>> {
+        const info = await this.deviceManager.getDeviceInfo(device);
+        if (semver.satisfies(info.osVersion || '', '>=4.0')) {
+            await this.luna.call(device, 'luna://com.webos.service.config/setConfigs', {configs: {"system.collectDevLogs": true}}, false);
+        } else if (semver.satisfies(info.osVersion || '', '>=2.0')) {
+            await this.luna.call(device, 'luna://com.webos.pmlogd/setdevlogstatus', {recordDevLogs: true}, false);
+        }
+        return this.log.logread(device, 100);
+    }
+
+    async openCtrl(): Promise<void> {
+        const device = this.device;
+        if (!device) {
+            return;
+        }
+        const progress = ProgressDialogComponent.open(this.modals);
+        try {
+            const contexts = await this.log.pmLogShow(device);
+            this.modals.open(PmLogControlComponent, {
+                injector: Injector.create({
+                    providers: [
+                        {provide: 'device', useValue: this.device},
+                        {provide: 'contexts', useValue: contexts}
+                    ]
+                }),
+                scrollable: true,
+            });
+        } finally {
+            progress.close();
+        }
+    }
+
+    async clearBuffer(): Promise<void> {
+        const device = this.device;
+        if (!device) {
+            return;
+        }
+        await this.log.logClear(device);
+        this.reload(device);
+    }
 }
--- src/app/types/luna-apis.ts
+++ src/app/types/luna-apis.ts
@@ -1,15 +1,20 @@
 import {LunaResponse} from "../core/services/remote-luna.service";
 
 export declare interface SystemInfo extends LunaResponse {
-  firmwareVersion: string;
-  modelName: string;
-  sdkVersion: string;
+    firmwareVersion: string;
+    modelName: string;
+    sdkVersion: string;
+}
+
+export declare interface OsInfo extends LunaResponse {
+    webos_manufacturing_version: string;
+    webos_release: string;
 }
 
 export declare interface HomebrewChannelConfiguration extends LunaResponse {
-  root: boolean,
-  telnetDisabled: boolean,
-  failsafe: boolean,
-  sshdEnabled: boolean,
-  blockUpdates: boolean
+    root: boolean,
+    telnetDisabled: boolean,
+    failsafe: boolean,
+    sshdEnabled: boolean,
+    blockUpdates: boolean
 }
