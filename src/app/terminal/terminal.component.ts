21e40920d7a010f243bf650f7d9fd6073260f227
Mariotaku <mariotaku.lee@gmail.com>
2023-11-22 15:28:50 +0900

    code cleanup

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -6,117 +6,117 @@ import {filter} from "rxjs/operators";
 import {isNonNull} from "../shared/operators";
 import {RemoteShellService, ShellInfo, ShellToken} from "../core/services/remote-shell.service";
 import {ITerminalDimensions} from "xterm-addon-fit";
-import {listen, UnlistenFn} from "@tauri-apps/api/event";
+import {listen, UnlistenFn, Event} from "@tauri-apps/api/event";
 import {ProgressDialogComponent} from "../shared/components/progress-dialog/progress-dialog.component";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {MessageDialogComponent} from "../shared/components/message-dialog/message-dialog.component";
 
 
 @Component({
-  selector: 'app-terminal-host',
-  templateUrl: './terminal.component.html',
-  styleUrls: ['./terminal.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-terminal-host',
+    templateUrl: './terminal.component.html',
+    styleUrls: ['./terminal.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class TerminalComponent implements OnInit, OnDestroy {
 
-  public shells: ShellInfo[] = [];
+    public shells: ShellInfo[] = [];
 
-  public currentShell?: ShellToken;
+    public currentShell?: ShellToken;
 
-  public termSize?: ITerminalDimensions;
+    public termSize?: ITerminalDimensions;
 
-  private subscription?: Subscription;
+    private subscription?: Subscription;
 
-  @ViewChildren('terminal')
-  public terminals?: QueryList<ITerminalComponent>;
+    @ViewChildren('terminal')
+    public terminals?: QueryList<ITerminalComponent>;
 
-  public preferDumbShell: boolean = false;
-  public pendingResize?: ITerminalDimensions;
-  private unlistenFns: UnlistenFn[] = [];
+    public preferDumbShell: boolean = false;
+    public pendingResize?: ITerminalDimensions;
+    private unlistenFns: UnlistenFn[] = [];
 
-  constructor(public deviceManager: DeviceManagerService, private shell: RemoteShellService,
-              private zone: NgZone, private modals: NgbModal) {
-  }
-
-  async ngOnInit(): Promise<void> {
-    this.shell.list().then(async (shells) => {
-      this.shells = shells;
-      if (shells.length) {
-        this.currentShell = shells[0].token;
-        return;
-      }
-    });
-    this.unlistenFns.push(await listen('shell-info', (event) =>
-      this.zone.run(() => this.updateTab(event.payload as ShellInfo))));
-    this.unlistenFns.push(await listen('shell-removed', (event) =>
-      this.zone.run(() => this.closeTab(event.payload as ShellToken, false))));
-  }
-
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
-    for (let unlistenFn of this.unlistenFns) {
-      unlistenFn();
+    constructor(public deviceManager: DeviceManagerService, private shell: RemoteShellService,
+                private zone: NgZone, private modals: NgbModal) {
     }
-  }
 
-  closeTab(token: ShellToken, sendClose: boolean = true) {
-    const tabIndex = this.shells.findIndex((i) => i.token === token);
-    if (tabIndex >= 0) {
-      this.shells.splice(tabIndex, 1);
+    async ngOnInit(): Promise<void> {
+        this.shell.list().then(async (shells) => {
+            this.shells = shells;
+            if (shells.length) {
+                this.currentShell = shells[0].token;
+                return;
+            }
+        });
+        this.unlistenFns.push(await listen('shell-info', (event: Event<ShellInfo>) =>
+            this.zone.run(() => this.updateTab(event.payload))));
+        this.unlistenFns.push(await listen('shell-removed', (event: Event<ShellToken>) =>
+            this.zone.run(() => this.closeTab(event.payload, false))));
     }
-    if (this.currentShell === token) {
-      this.currentShell = this.shells[tabIndex > 0 ? tabIndex - 1 : 0]?.token;
-    }
-    if (sendClose) {
-      this.shell.close(token).then(noop);
-    }
-  }
 
-  updateTab(info: ShellInfo) {
-    const tabIndex = this.shells.findIndex((i) => i.token === info.token);
-    if (tabIndex < 0) return;
-    this.shells[tabIndex] = info;
-  }
-
-  async newTab(device?: Device): Promise<void> {
-    const size = this.termSize;
-    if (!size) {
-      return;
+    ngOnDestroy(): void {
+        this.subscription?.unsubscribe();
+        for (let unlistenFn of this.unlistenFns) {
+            unlistenFn();
+        }
     }
-    const startWith = device ?? await firstValueFrom(this.deviceManager.selected$.pipe<Device>(filter(isNonNull)));
-    const progress = ProgressDialogComponent.open(this.modals);
-    try {
-      const shellInfo = await this.shell.open(startWith, size.rows, size.cols, this.preferDumbShell);
-      this.shells.push(shellInfo);
-      this.currentShell = shellInfo.token;
-    } catch (e) {
-      MessageDialogComponent.open(this.modals, {
-        title: 'Failed to open terminal',
-        message: 'Please make sure the TV was turned on. For some cases, you will need to wait one minute or two for SSH to be available.',
-        error: e as Error,
-        positive: 'Close'
-      });
-    } finally {
-      progress.close();
-    }
-  }
 
-  shellTracker(index: number, value: ShellInfo): string {
-    return value.token;
-  }
-
-  focusShell(shell: ShellInfo) {
-    const match = this.terminals?.find(item => item.token === shell.token);
-    if (!match) {
-      return;
+    closeTab(token: ShellToken, sendClose: boolean = true) {
+        const tabIndex = this.shells.findIndex((i) => i.token === token);
+        if (tabIndex >= 0) {
+            this.shells.splice(tabIndex, 1);
+        }
+        if (this.currentShell === token) {
+            this.currentShell = this.shells[tabIndex > 0 ? tabIndex - 1 : 0]?.token;
+        }
+        if (sendClose) {
+            this.shell.close(token).then(noop);
+        }
     }
-    match.focus();
-  }
+
+    updateTab(info: ShellInfo) {
+        const tabIndex = this.shells.findIndex((i) => i.token === info.token);
+        if (tabIndex < 0) return;
+        this.shells[tabIndex] = info;
+    }
+
+    async newTab(device?: Device): Promise<void> {
+        const size = this.termSize;
+        if (!size) {
+            return;
+        }
+        const startWith = device ?? await firstValueFrom(this.deviceManager.selected$.pipe<Device>(filter(isNonNull)));
+        const progress = ProgressDialogComponent.open(this.modals);
+        try {
+            const shellInfo = await this.shell.open(startWith, size.rows, size.cols, this.preferDumbShell);
+            this.shells.push(shellInfo);
+            this.currentShell = shellInfo.token;
+        } catch (e) {
+            MessageDialogComponent.open(this.modals, {
+                title: 'Failed to open terminal',
+                message: 'Please make sure the TV was turned on. For some cases, you will need to wait one minute or two for SSH to be available.',
+                error: e as Error,
+                positive: 'Close'
+            });
+        } finally {
+            progress.close();
+        }
+    }
+
+    shellTracker(_index: number, value: ShellInfo): string {
+        return value.token;
+    }
+
+    focusShell(shell: ShellInfo) {
+        const match = this.terminals?.find(item => item.token === shell.token);
+        if (!match) {
+            return;
+        }
+        match.focus();
+    }
 }
 
 export interface ITerminalComponent {
-  readonly token: ShellToken;
+    readonly token: ShellToken;
 
-  focus(): void;
+    focus(): void;
 }
