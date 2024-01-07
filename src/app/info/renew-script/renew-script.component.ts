d9f5c0623bef7135453596a91f4d48bf35deba5b
Mariotaku <mariotaku.lee@gmail.com>
2024-01-07 11:48:06 +0900

    Notice for IFTTT change of feature #175

--- src/app/info/renew-script/renew-script.component.ts
+++ src/app/info/renew-script/renew-script.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {Device} from '../../types';
-import {DeviceManagerService, DevModeService} from '../../core/services';
+import {DeviceManagerService, DevModeStatus} from '../../core/services';
 import {noop} from 'rxjs';
 import {save as showSaveDialog} from '@tauri-apps/api/dialog'
 import {writeTextFile} from '@tauri-apps/api/fs';
@@ -9,47 +9,45 @@ import renewScriptTemplate from './renew-script.sh';
 import Mustache from 'mustache';
 
 @Component({
-  selector: 'app-renew-script',
-  templateUrl: './renew-script.component.html',
-  styleUrls: ['./renew-script.component.scss']
+    selector: 'app-renew-script',
+    templateUrl: './renew-script.component.html',
+    styleUrls: ['./renew-script.component.scss']
 })
 export class RenewScriptComponent implements OnInit {
 
-  public renewScriptContent?: string;
-  public devModeToken?: string;
+    public renewScriptContent?: string;
 
-  constructor(
-    public modal: NgbActiveModal,
-    private deviceManager: DeviceManagerService,
-    private devMode: DevModeService,
-    @Inject('device') public device: Device
-  ) {
-  }
+    constructor(
+        public modal: NgbActiveModal,
+        private deviceManager: DeviceManagerService,
+        @Inject('device') public device: Device,
+        @Inject('devMode') public devMode: DevModeStatus,
+    ) {
+    }
 
-  ngOnInit(): void {
-    this.devMode.status(this.device).then(({token}) => this.devModeToken = token);
-    this.deviceManager.readPrivKey(this.device).then(key => {
-      this.renewScriptContent = Mustache.render(renewScriptTemplate, {
-        device: this.device,
-        keyContent: key.trim(),
-      }, undefined, {
-        escape: (v) => v,
-      });
-    });
-  }
+    ngOnInit(): void {
+        this.deviceManager.readPrivKey(this.device).then(key => {
+            this.renewScriptContent = Mustache.render(renewScriptTemplate, {
+                device: this.device,
+                keyContent: key.trim(),
+            }, undefined, {
+                escape: (v) => v,
+            });
+        });
+    }
 
-  async copyScript(content: string): Promise<void> {
-    await navigator.clipboard.writeText(content);
-  }
+    async copyScript(content: string): Promise<void> {
+        await navigator.clipboard.writeText(content);
+    }
 
-  saveScript(content: string): void {
-    showSaveDialog({
-      defaultPath: `renew-devmode-${this.device.name}.sh`
-    }).then(value => {
-      if (!value) {
-        return;
-      }
-      return writeTextFile(value, content);
-    }).catch(noop);
-  }
+    saveScript(content: string): void {
+        showSaveDialog({
+            defaultPath: `renew-devmode-${this.device.name}.sh`
+        }).then(value => {
+            if (!value) {
+                return;
+            }
+            return writeTextFile(value, content);
+        }).catch(noop);
+    }
 }
