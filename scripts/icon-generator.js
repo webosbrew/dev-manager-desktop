fc6d56df71c4cca72cd0533b1e06ec0765c84811
Mariotaku <mariotaku.lee@gmail.com>
2024-05-16 13:46:54 +0900

    use prebuilt openssl for Android on Windows

--- scripts/icon-generator.js
+++ scripts/icon-generator.js
@@ -12,61 +12,93 @@ mkdirSync('tmp', {recursive: true});
 const tmpDesktopIcons = mkdtempSync(path.join('tmp', 'icons-desktop-'));
 const tmpMobileIcons = mkdtempSync(path.join('tmp', 'icons-mobile-'));
 
-async function generateIcons() {
-  // Generate desktop icons
-  await cli.run(['icon', '-o', tmpDesktopIcons, 'resources/icon/desktop.svg'], null);
-  await cli.run(['icon', '-o', tmpMobileIcons, '--ios-color', '#03A9F4', 'resources/icon/mobile.png'], null);
+function matchesPlatform(platforms, platform) {
+  return !platforms || !platforms.length || platforms.includes(platform);
 }
 
-async function copyIcons() {
+async function generateIcons(platforms) {
+  // Generate desktop icons
+  if (matchesPlatform(platforms, 'desktop')) {
+    await cli.run(['icon', '-o', tmpDesktopIcons, 'resources/icon/desktop.svg'], null);
+  }
+  if (matchesPlatform(platforms, 'android') || matchesPlatform(platforms, 'ios')) {
+    await cli.run(['icon', '-o', tmpMobileIcons, '--ios-color', '#03A9F4', 'resources/icon/mobile.png'], null);
+  }
+}
+
+async function copyIcons(platforms) {
   function iconPathSegs(source, base) {
     return source.substring(source.lastIndexOf(base) + base.length + 1).split(path.sep);
   }
 
   // Copy desktop icons
-  await fs.cp(tmpDesktopIcons, 'src-tauri/icons', {
-    recursive: true,
-    filter(source, _destination) {
-      const segs = iconPathSegs(source, tmpDesktopIcons);
-      return segs[0] !== 'android' && segs[0] !== 'ios';
-    }
-  });
-
-  // Copy iOS icons
-  await fs.cp(tmpMobileIcons, 'src-tauri/icons', {
-    recursive: true,
-    filter(source, _destination) {
-      const segs = iconPathSegs(source, tmpMobileIcons);
-      return !segs[0] || segs[0] === 'ios';
-    }
-  });
-
-  // Clean Android icons
-  const androidResDir = 'src-tauri/gen/android/app/src/main/res';
-  for (const file of await fs.readdir(androidResDir, {recursive: true})) {
-    const segs = file.split(path.sep);
-    if (segs.length === 2 && (segs[0].startsWith('mipmap') || segs[0].startsWith('drawable')) && segs[1].startsWith('ic_launcher')) {
-      await fs.rm(path.join(androidResDir, file));
-    }
+  if (matchesPlatform(platforms, 'desktop')) {
+    await fs.cp(tmpDesktopIcons, 'src-tauri/icons', {
+      recursive: true,
+      filter(source, _destination) {
+        const segs = iconPathSegs(source, tmpDesktopIcons);
+        return segs[0] !== 'android' && segs[0] !== 'ios';
+      }
+    });
   }
 
-  // Copy ic_launcher, ic_launcher_background
-  await fs.cp(path.join('resources/icon/android/main'), path.join(androidResDir), {recursive: true});
-
-  // Copy ic_launcher_foreground
-  await fs.cp(path.join(tmpMobileIcons, 'android'), androidResDir, {
-    recursive: true,
-    filter(source, _destination) {
-      const segs = iconPathSegs(source, tmpMobileIcons);
-      if (segs[0] && segs[0] !== 'android') {
-        return false;
+  // Copy iOS icons
+  if (matchesPlatform(platforms, 'ios')) {
+    await fs.cp(tmpMobileIcons, 'src-tauri/icons', {
+      recursive: true,
+      filter(source, _destination) {
+        const segs = iconPathSegs(source, tmpMobileIcons);
+        return !segs[0] || segs[0] === 'ios';
       }
-      return !segs[2] || segs[2] === 'ic_launcher_foreground.png';
+    });
+  }
+
+  // Clean Android icons
+  if (matchesPlatform(platforms, 'android')) {
+    const androidResDir = 'src-tauri/gen/android/app/src/main/res';
+    for (const file of await fs.readdir(androidResDir, {recursive: true})) {
+      const segs = file.split(path.sep);
+      if (segs.length === 2 && (segs[0].startsWith('mipmap') || segs[0].startsWith('drawable')) && segs[1].startsWith('ic_launcher')) {
+        await fs.rm(path.join(androidResDir, file));
+      }
     }
-  });
+
+    // Copy ic_launcher, ic_launcher_background
+    await fs.cp(path.join('resources/icon/android/main'), path.join(androidResDir), {recursive: true});
+
+    // Copy ic_launcher_foreground
+    await fs.cp(path.join(tmpMobileIcons, 'android'), androidResDir, {
+      recursive: true,
+      filter(source, _destination) {
+        const segs = iconPathSegs(source, tmpMobileIcons);
+        if (segs[0] && segs[0] !== 'android') {
+          return false;
+        }
+        return !segs[2] || segs[2] === 'ic_launcher_foreground.png';
+      }
+    });
+  }
 }
 
-generateIcons().then(() => copyIcons()).finally(() => {
-  rmSync(tmpDesktopIcons, {recursive: true});
-  rmSync(tmpMobileIcons, {recursive: true});
-});
+/**
+ * @param {string} platforms
+ */
+async function run(...platforms) {
+  console.log('Generating icons for platforms:', platforms?.join(', ') || 'all');
+  try {
+    await generateIcons(platforms);
+    await copyIcons(platforms);
+  } finally {
+    rmSync(tmpDesktopIcons, {recursive: true});
+    rmSync(tmpMobileIcons, {recursive: true});
+  }
+}
+
+if (require.main === module) {
+  run(...process.argv.slice(2)).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+} else {
+  module.exports = {run};
+}
--- scripts/tauri-wrapper.js
+++ scripts/tauri-wrapper.js
@@ -1,11 +1,86 @@
 #!/usr/bin/env node
 'use strict';
 
-if (process.platform === 'win32' && process.argv[2] === 'android') {
-  process.env['PERL'] = 'C:/msys64/usr/bin/perl.exe';
+const https = require('https');
+const fs = require('fs');
+const child_process = require('child_process');
+const path = require('path');
+const cli = require('@tauri-apps/cli');
 
-  const ndkPath = process.env['NDK_HOME'] + '\\toolchains\\llvm\\prebuilt\\windows-x86_64\\bin';
-  process.env['Path'] = process.env['Path'] + ';C:/msys64/usr/bin;' + ndkPath;
+async function downloadFile(url, dest) {
+  const fileHandle = fs.openSync(dest, 'w');
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode === 302 || res.statusCode === 301) {
+        return downloadFile(res.headers.location, dest).then(resolve).catch(reject);
+      }
+      if (res.statusCode !== 200) {
+        return reject(new Error(`Failed to download file: ${res.statusCode}`));
+      }
+      res.setEncoding('binary');
+      res.on('data', (data) => {
+        fs.appendFileSync(fileHandle, data, {encoding: 'binary'});
+      });
+      res.on('end', resolve);
+      res.on('error', reject);
+    });
+  }).finally(() => fs.closeSync(fileHandle));
 }
 
-require('@tauri-apps/cli/tauri');
+async function prepareOpenSSL() {
+  await fs.promises.mkdir('tmp', {recursive: true});
+  if (!await fs.promises.access('tmp/android_openssl-master').then(() => true).catch(() => false)) {
+    await downloadFile('https://github.com/KDAB/android_openssl/archive/refs/heads/master.tar.gz', 'tmp/android_openssl-master.tar.gz');
+    await new Promise((resolve, reject) => {
+      child_process.exec('tar -xzf android_openssl-master.tar.gz', {cwd: 'tmp'}, (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    }).catch(async (err) => {
+      await fs.rm('tmp/android_openssl-master', {recursive: true, force: true});
+      throw err;
+    });
+  }
+  const opensslDir = path.resolve('tmp', 'android_openssl-master', 'ssl_1.1');
+  process.env['OPENSSL_NO_VENDOR'] = '1';
+  process.env['OPENSSL_INCLUDE_DIR'] = path.join(opensslDir, 'include');
+  process.env['AARCH64_LINUX_ANDROID_OPENSSL_LIB_DIR'] = path.join(opensslDir, 'arm64-v8a');
+  process.env['ARMV7_LINUX_ANDROIDEABI_OPENSSL_LIB_DIR'] = path.join(opensslDir, 'armeabi-v7a');
+  process.env['I686_LINUX_ANDROID_OPENSSL_LIB_DIR'] = path.join(opensslDir, 'x86');
+  process.env['X86_64_LINUX_ANDROID_OPENSSL_LIB_DIR'] = path.join(opensslDir, 'x86_64');
+}
+
+async function copyOpenSSLLibs() {
+  const opensslDir = path.resolve('tmp', 'android_openssl-master', 'ssl_1.1');
+  await fs.promises.mkdir('src-tauri/gen/android/app/src/main/jniLibs', {recursive: true});
+  await fs.promises.cp(path.join(opensslDir), 'src-tauri/gen/android/app/src/main/jniLibs', {
+    recursive: true,
+    async filter(source, destination) {
+      if ((await fs.promises.lstat(source)).isSymbolicLink()) {
+        return false;
+      }
+      return !source.endsWith('include') && !source.endsWith('.a') && !source.endsWith('.h');
+    }
+  });
+}
+
+async function run() {
+  if (process.platform === 'win32' && process.argv[2] === 'android') {
+    await prepareOpenSSL();
+  }
+  await cli.run(process.argv.slice(2), null);
+
+}
+
+run().then(async () => {
+  if (process.argv[2] === 'android' && process.argv[3] === 'init') {
+    const icoGen = require('./icon-generator');
+    await icoGen.run('android');
+    if (process.platform === 'win32') {
+      await copyOpenSSLLibs();
+    }
+  }
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
