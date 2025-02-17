6e9167ef3e6e307cf1b7724874ee5cfcb5a53432
Mariotaku <mariotaku.lee@gmail.com>
2025-02-17 14:05:08 +0900

    improved process output

--- fastlane/util/npm.rb
+++ fastlane/util/npm.rb
@@ -1,7 +1,7 @@
 module NPM
   def self.run(command, *args)
-    pid = Process.spawn("npm", "--prefix", "..", "run", command, "--", *args)
-    _, status = Process.wait2(pid)
-    raise "npm failed with status #{status}" if status != 0
+    Dir.chdir("..") do
+      Fastlane::Actions::sh("npm", "run", command, "--", *args)
+    end
   end
 end
